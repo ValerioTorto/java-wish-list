9f65861fa1d0b5d23283c5306eac1b7d313e2a64
ValerioTorto <valerio.tortorelli@gmail.com>
2023-12-07 19:01:29 +0100

    First exercise done v2

--- src/ChristmasPresents/Presents.java
+++ src/ChristmasPresents/Presents.java
@@ -31,43 +31,8 @@ public class Presents {
                     stop = true;
                     break;
             }
-
-
-       /*
-        //creo la lista dei regali
-        ArrayList<String> presents = new ArrayList<>();
-        System.out.println(presents.size());
-        boolean stop = false;
-        while (!stop) {
-            // chiedo se vuole inserire un nuovo regalo
-            System.out.print("Add a new present? (y/n)");
-            String answer = scanner.nextLine();
-            switch (answer) {
-                case "y":
-                    // inserisce nuovo regalo
-                    System.out.print("Insert present: ");
-                    String present = scanner.nextLine();
-                    System.out.println(presents);
-                    // aggiungo il nome alla lista
-                    presents.add(present);
-
-                    boolean added = presents.add(present);
-                    if(!added){
-                        System.out.println("Present already added");
-                    }
-
-                    break;
-                case "n":
-                    // esce
-                    System.out.println("Here are your presents: " + presents);
-                    stop = true;
-                    break;
-                default:
-            }
-        }*/
         }
         System.out.println(presents);
         scanner.close();
-
     }
 }
