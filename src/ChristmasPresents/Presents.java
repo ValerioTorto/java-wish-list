b105bc3d6339f5a1fd4e2517ca8524cece7db2d3
ValerioTorto <valerio.tortorelli@gmail.com>
2023-12-07 19:00:58 +0100

    First exercise done

--- src/ChristmasPresents/Presents.java
+++ src/ChristmasPresents/Presents.java
@@ -1,11 +1,39 @@
 package ChristmasPresents;
 
-import java.util.ArrayList;
+
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Presents {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //creo insieme di regali senza duplicati
+        HashSet<String> presents = new HashSet<>();
+
+        boolean stop = false;
+        while (!stop) {
+            System.out.println("insert new present? (y/n)");
+            String answer = scanner.nextLine();
+            switch (answer) {
+                case "y":
+                    //aggiungo regalo
+                    System.out.println("Present: ");
+                    String present = scanner.nextLine();
+                    boolean added = presents.add(present);
+                    System.out.println(presents);
+
+
+                    if (!added) {
+                        System.out.println("Present already added");
+                    }
+                    break;
+                case "n":
+                    stop = true;
+                    break;
+            }
+
+
+       /*
         //creo la lista dei regali
         ArrayList<String> presents = new ArrayList<>();
         System.out.println(presents.size());
@@ -36,6 +64,10 @@ public class Presents {
                     break;
                 default:
             }
+        }*/
         }
+        System.out.println(presents);
+        scanner.close();
+
     }
 }
